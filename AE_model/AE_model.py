345ca075da38f544642525aa16650ae0352897f5
Adrian <amarkelo@andrew.cmu.edu>
2018-07-09 15:01:21 -0400

    asdfasdf

--- AE_model/AE_model.py
+++ AE_model/AE_model.py
@@ -13,7 +13,7 @@ plt.ioff()
 testing_batch_size = 64
 training_batch_size = 64
 num_validation_samples = 64
-epochs = 3
+epochs = 500
 plot_rows = 4 # num of test image rows to run and plot
 plot_cols = 6 # ditto (cols must be even)
 add_skip_connect = False # if true will learn ID function in like 2 epochs
@@ -56,28 +56,27 @@ def new_conv_up_layer(input,num_input_channels,filter_size,num_filters):
     input_img_size = int(input_shape[1])
     output_img_shape = np.array([input_img_size*2, input_img_size*2], np.int32)
     
-    weights = new_weights(shape=shape)
-    biases = new_biases(length=num_filters)
+    #weights = new_weights(shape=shape)
+    #biases = new_biases(length=num_filters)
     
-    upsample_imgs = tf.image.resize_images(images=input, size=output_img_shape)
+    upsample_imgs = tf.image.resize_images(images=input, size=output_img_shape,method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     
-    layer = tf.nn.conv2d(input=upsample_imgs,filter=weights, strides=[1,1,1,1], padding='SAME')
-    layer += biases
-    layer = tf.nn.relu(layer)
+    #layer = tf.nn.conv2d(input=upsample_imgs,filter=weights, strides=[1,1,1,1], padding='SAME')
+    #layer += biases
+    
+    layer = tf.layers.conv2d(inputs=upsample_imgs, filters=num_filters, kernel_size=(filter_size,filter_size), padding='same', activation=tf.nn.relu)
     return layer
 
 def new_conv_trans_layer(input,num_input_channels,filter_size,num_filters, stride=2):
-    #layer = tf.layers.conv2d_transpose(inputs=inputs,filters=num_filters, kernel_size=[2,2], strides=2)
+    #layer = tf.layers.conv2d_transpose(inputs=input,filters=num_filters, kernel_size=[2,2], strides=2)
     input_shape = input.get_shape().as_list()
     input_img_size = int(input_shape[1])
-    k_shape = [filter_size, filter_size, num_filters, num_input_channels]
+    k_shape = [2, 2, num_filters, num_input_channels]
     output_shape = np.array([training_batch_size, input_img_size*2, input_img_size*2, num_filters], np.int32)
     
     weights = new_weights(shape=k_shape)
     biases = new_biases(length=num_filters)
     
-    print(type(output_shape))
-    print(output_shape)
     
     # conv2d_transpose: 
     #  output_size = strides * (input_size-1) + kernel_size - 2*padding
@@ -101,7 +100,7 @@ def build_net(x, add_skip_connect):
     conv_layer_2 = new_conv_layer(conv_layer_1, num_filters_l1, filter_size, num_filters_l2, use_pooling=True)    
     
     # Up Sampling
-    conv_layer_3 = new_conv_trans_layer(conv_layer_2 ,num_filters_l2, filter_size, num_filters_l3)
+    conv_layer_3 = new_conv_up_layer(conv_layer_2, num_filters_l2, filter_size, num_filters_l3)
     # Add conv_layer_1 and conv_layer_3 together (both are (14,14) images)
     if(add_skip_connect):
         print('conv_layer_3 in skip connected. Shape should be: [%s + %s]'%(conv_layer_3.get_shape(), conv_layer_1.get_shape()))
@@ -109,7 +108,7 @@ def build_net(x, add_skip_connect):
         conv_layer_3_shape = conv_layer_3.get_shape().as_list()
         num_filters_l3 = tf.cast(conv_layer_3_shape[3], tf.int32)
     
-    output = new_conv_trans_layer(conv_layer_3, num_filters_l3, filter_size, num_filters_output)
+    output = new_conv_up_layer(conv_layer_3, num_filters_l3, filter_size, num_filters_output)
     output = tf.nn.relu(output, name='output')
     
     # save an optimizer for the given graph above
